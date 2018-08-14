72fe4d4fc323fbcc21971abae2f7dc3909e47fe7
phillirodney <phillirodney@gmail.com>
2018-08-14 22:11:48 +0100

    Header component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
+        <div className="App-header">
+          <h2>Discover Your Movie Mojo!</h2>
+        </div>
         <p className="App-intro">
-          Welcome to the 'Movie Mojo' React App.
+          Welcome to the 'Movie Mojo' React app!
         </p>
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return React.createElement( 'div', null, 'Hello there, this is a React component!' );
+    }
+}
+ 
+export default Header;
